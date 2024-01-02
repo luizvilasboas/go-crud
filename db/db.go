e3f886428b83a50e5371989de9d1691ba4783986
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-01-02 15:03:16 -0300

    Add .env file for configuration

--- db/db.go
+++ db/db.go
@@ -2,13 +2,25 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func ConnectDatabase() *sql.DB {
-	db, err := sql.Open("postgres", "user=luiz password=root dbname=alura_loja host=localhost sslmode=disable")
+	envFile := ".env.local"
+
+	err := godotenv.Load(envFile)
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable", os.Getenv("POSTGRESQL_NAME"), os.Getenv("POSTGRESQL_PASSWORD"), os.Getenv("POSTGRESQL_DBNAME"), os.Getenv("POSTGRESQL_HOST"))
+
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Error connecting to the database:", err)
 	}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module gitlab.com/alura-courses-code/golang/web-crud
 go 1.21.1
 
 require github.com/lib/pq v1.10.9
+
+require github.com/joho/godotenv v1.5.1
